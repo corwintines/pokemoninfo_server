b65e626095bcb5e97d028bfc9cd48c5e9457ff30
Corwin Smith <corwin.smith@uleth.ca>
2018-10-06 15:30:49 -0600

    Concert app.js to module

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from 'experss'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import pokemon_routing from './api/pokemon'
 
 const app = express();
 
-const pokemon_routing = require('./api/pokemon');
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/api/v1/pokemon', pokemon_routing);
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/api/v1/pokemon', pokemon_routing)
 
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.json({
     message: err.message,
     error: req.app.get('env') === 'development' ? err : {}
-  });
-});
+  })
+})
 
-const PORT = process.env.PORT || 8090;
-app.listen(PORT);
-console.log(`App is listening on port ${PORT}`);
+module.exports = app;
